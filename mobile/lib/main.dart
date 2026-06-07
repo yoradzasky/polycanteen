9dfce7f759d2085e4d531113d1581ca8391611f7
asdiffada <afada085@gmail.com>
2026-06-07 13:19:47 +0700

    fix

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 import 'core/auth/screens/login_screen.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
